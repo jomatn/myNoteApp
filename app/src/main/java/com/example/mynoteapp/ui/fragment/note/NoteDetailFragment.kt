bdd775db86542c8de2a226bc74fc3a7d47f0a4e1
jomatn <156526863+jomatn@users.noreply.github.com>
2024-04-29 08:23:06 +0600

    Update NoteDetailFragment.kt

--- app/src/main/java/com/example/mynoteapp/ui/fragment/note/NoteDetailFragment.kt
+++ app/src/main/java/com/example/mynoteapp/ui/fragment/note/NoteDetailFragment.kt
@@ -5,53 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.mynoteapp.ARG_PARAM1
-import com.example.mynoteapp.ARG_PARAM2
-import com.example.mynoteapp.R
+import com.example.mynoteapp.Rclass NoteDetailFragment : Fragment() {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [NoteDetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class NoteDetailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentNoteDetailBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_note_detail, container, false)
+    ): View {
+        binding = FragmentNoteDetailBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NoteDetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NoteDetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnAddText.setOnClickListener{
+            val title = binding.etAdd.text.toString()
+            App().getInstance()?.noteDao()?.insertNote(NoteModel(title))
+            findNavController().navigateUp(
+
+            )
+
+      
+        }
     }
 }
